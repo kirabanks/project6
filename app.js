336ac0c3cc65a2ad79f9e0b888b328d84cb1448e
papr <92340521+kirabanks@users.noreply.github.com>
2024-05-01 11:05:36 -0400

    Hide Start Screen
    Created an Event Listener to hide the start screen overlay when the "start game" button was clicked.

--- app.js
+++ app.js
@@ -11,5 +11,13 @@ const missed = 0;
 // Populate with 5 different phrases as strings
 const phrases = ["cut to the chase", "busy bee", "close but no cigar", "ugly duckling", "burst your bubble"];
 
+// Hide the Start Screen Overlay
+// Attach an event listener to the “Start Game” button to hide the start screen overlay when clicked. Clicking the button should now make the on-screen keyboard visible.
+const startButton = document.querySelector('.btn__reset');
+startButton.addEventListener('click', () => {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+});
+
 // Testing
 console.log(phrases);
