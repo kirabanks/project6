ded3880cb3646d9b4eb8db18c0cf9d873d755805
papr <92340521+kirabanks@users.noreply.github.com>
2024-05-01 11:04:14 -0400

    Created "phrases" Array
    Created a "phrases" array, and stored 5 random phrases as strings.

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ const phrase = document.getElementById('phrase');
 // Create a "missed" variable, initialized to 0.
 const missed = 0;
 
+// Create a "phrases" array.
+// Populate with 5 different phrases as strings
+const phrases = ["cut to the chase", "busy bee", "close but no cigar", "ugly duckling", "burst your bubble"];
+
 // Testing
-console.log(qwerty);
+console.log(phrases);
