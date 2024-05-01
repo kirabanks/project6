6457c3f81c76e32dfee204f7f06f5b2482a9601d
papr <92340521+kirabanks@users.noreply.github.com>
2024-05-01 13:02:41 -0400

    New function added to get random phrase
    Created a new function "getRandomPhraseAsArray" that selects a random phrase from the previous "phrases" array and stores it in "randomPhrase".
    
    Created a new variable called "phraseLength" that determines how many characters are in the randomly chosen phrase, including spaces.
    
    Created a new variable that splits the chosen phrase into separate characters and stores in a new string.

--- app.js
+++ app.js
@@ -8,16 +8,27 @@ const phrase = document.getElementById('phrase');
 const missed = 0;
 
 // Create a "phrases" array.
-// Populate with 5 different phrases as strings
 const phrases = ["cut to the chase", "busy bee", "close but no cigar", "ugly duckling", "burst your bubble"];
 
-// Hide the Start Screen Overlay
-// Attach an event listener to the “Start Game” button to hide the start screen overlay when clicked. Clicking the button should now make the on-screen keyboard visible.
+// Hide the Start Screen Overlay with start button
 const startButton = document.querySelector('.btn__reset');
 startButton.addEventListener('click', () => {
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
 
+// Create a "getRandomPhraseAsArray" function that chooses a random phrase, splits the phrase into a new array of characters, and returns the phrase into a new array.
+function getRandomPhraseAsArray() {
+    // Randomly choose a phrase from "phrases"
+    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+    // Variable holding the length of the selected phrase.
+    const phraseLength = randomPhrase.length;
+    // Create a new array holding a string of each charcter of randomPhrase including spaces.
+    const newPhraseArray = randomPhrase.split("");
+    // Returns the new array.
+    return newPhraseArray;
+}
+
+
 // Testing
-console.log(phrases);
+console.log(getRandomPhraseAsArray());
