d8819d5b9a83fe711659bdf83c5b8803422ac48e
papr <92340521+kirabanks@users.noreply.github.com>
2024-05-01 11:01:09 -0400

    Starting Project
    Downloaded project files, and started step 1 of adding variables "qwerty" and "phrase".

---
+++ app.js
@@ -0,0 +1,8 @@
+// Get Element ID "qwerty"
+const qwerty = document.getElementById('qwerty');
+
+// Get Element ID "phrase"
+const phrase = document.getElementById('phrase');
+
+// Testing
+console.log(qwerty);
