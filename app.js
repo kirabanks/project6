321a9f35d74bdf4d8dcfc36e1b28f0ecb4dd9741
papr <92340521+kirabanks@users.noreply.github.com>
2024-05-01 17:20:20 -0400

    Cleaned up notes
    Cleaned up notes and indentation.

--- app.js
+++ app.js
@@ -1,44 +1,39 @@
-// Get Element ID "qwerty"
 const qwerty = document.getElementById('qwerty');
-
-// Get Element ID "phrase"
 const phrase = document.getElementById('phrase');
-
-// Create a "missed" variable, initialized to 0.
+// Set player misses to 0.
 let missed = 0;
 
-// Create a "phrases" array.
+// Array that holds the phrases.
 const phrases = ["cut to the chase", "busy bee", "close but no cigar", "ugly duckling", "burst your bubble"];
 
-// Hide the Start Screen Overlay with start button
+// Hides the start screen overlay when button is clicked.
 const startButton = document.querySelector('.btn__reset');
 startButton.addEventListener('click', () => {
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
 
-// Create a "getRandomPhraseAsArray" function that chooses a random phrase, splits the phrase into a new array of characters, and returns the phrase into a new array.
-// Has to work on any array
+// Function that chooses a random phrase, splits the phrase into a new array of characters, and returns the phrase into a new array.
 function getRandomPhraseAsArray(arr) {
-    // Randomly choose a phrase from "phrases" and associate with a number based on length.
+    // Randomly choose a phrase and associate it with a number based on length.
     let randomNumber = Math.floor(Math.random() * arr.length);
     // Variable holding the random phrase and number
     let randomPhrase = arr[randomNumber];
-    // Create a new array holding a string of each charcter of randomPhrase including spaces.
+    // Array holding a string of each charcter including spaces.
     const splitPhrase = randomPhrase.split("");
     // Returns the new array.
     return splitPhrase;
 }
 
-// Create an "addPhraseToDisplay" function that loops through an array of characters.
+// Function that loops through the array of characters.
 const phraseArray = getRandomPhraseAsArray(phrases);
 const phraseLI = document.querySelector('ul');
 
 function addPhraseToDisplay(arr) {
-    // Write a loop that creates a li for each character.
+    // Loop that creates a list item for each character.
     for (let i = 0; i < arr.length; i++) {
         const list = document.createElement('li');
-        // Put Character inside of list item
+        // Puts character inside of list item
         let text = arr[i];
         list.textContent = text;
         // Label if the character is a space or letter.
@@ -47,14 +42,14 @@ function addPhraseToDisplay(arr) {
         } else {
             list.className = 'letter';
         }
-        // Append to the #phrase ul
+        // Append to the overall list.
         phraseLI.appendChild(list);
     }
 }
 
 addPhraseToDisplay(phraseArray);
 
-// Create "checkLetter" function that runs on click.
+// Function that checks the letter on click.
 function checkLetter(clicked) {
     // Variable storing all li elements with the class "letter"
     const letterChecked = document.getElementsByClassName('letter');
@@ -71,7 +66,7 @@ function checkLetter(clicked) {
 }
 
 
-// Add an event listener to the keyboard
+// Add an event listener to the on screen keyboard.
 qwerty.addEventListener('click', (e) => {
     // If a player has chosen a letter it gets a "chosen" class and becomes disabled to choose it again.
     if (e.target.tagName === 'BUTTON') {
@@ -79,19 +74,19 @@ qwerty.addEventListener('click', (e) => {
         e.target.disabled = 'true';
         // Pass to checkLetter function, and store in variable.
         const letterFound = checkLetter(e.target);
-        // Check if the value of letterFOund is null
+        // Check if the value of letter is null
         if (letterFound === null) {
-            // Remove heart from scoreboard and increase "missed"
+            // Remove heart from scoreboard and increase misses
             missed ++;
             const lives = document.querySelectorAll('img');
             lives[missed -1].src = 'images/lostHeart.png';
         }
     }
-    // Run checkWin function after each guess.
+    // Runs the function to check if player won after each guess.
     checkWin();
 });
 
-// Create function that checks whether the game has been won or lost.
+// Function that checks whether the game has been won or lost.
 function checkWin() {
 
     const letter = document.getElementsByClassName('letter');
